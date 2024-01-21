6d2b06f73d80aa618c0943f1a50d3fca89eadbfb
league <karinabriellen@gmail.com>
2024-01-21 11:26:51 -0800

    worked on riddles

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,27 @@
-
+PImage pepperoni;
+PImage mushroom;
+PImage olive;
 void setup() {
-    
+    size(800,800);
+    pepperoni = loadImage("pepperoni.png");
+    mushroom = loadImage("mushroom.png");
+    olive = loadImage("olive.png");
 }
 void draw() {
-    
+    ellipse(400, 400, 700, 700);
+    fill(212, 93, 53);
+    ellipse(400, 400, 600, 600);
+    fill(237, 183, 66);
+    ellipse(400,400,570,570);
+    fill(242, 194, 138);
+
+  if(mousePressed){
+    image(pepperoni,200,230);
+    image(mushroom,300,390);
+    image(pepperoni,450,390);
+    image(pepperoni,340,560);
+    image(mushroom,500,300);
+    image(olive,180,500);
+    image(olive,400,170);
+}
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,24 +2,62 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int count = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+String input = JOptionPane.showInputDialog("When is a door no longer a door?");
+System.out.println(input);
+if (input.equals("When its ajar")) {
+	JOptionPane.showMessageDialog(null, "correct");
+	count+=1;
+}
+
+else {
+	JOptionPane.showMessageDialog(null, "incorrect");
+}
+
+
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
+String input1 = JOptionPane.showInputDialog("What do you buy to eat but never consume?");
+System.out.println(input1);
+if (input1.equals("Cutlery")) {
+	JOptionPane.showMessageDialog(null, "correct");
+	count+=1;
+}
+
+else {
+	JOptionPane.showMessageDialog(null, "incorrect");
+	count-=1;
+}
+
+
+String input2 = JOptionPane.showInputDialog("What kind of room has no walls, door or windows?");
+System.out.println(input2);
+if (input2.equals("A mushroom")) {
+	JOptionPane.showMessageDialog(null, "correct");
+	count+=1;
+}
+
+else {
+	JOptionPane.showMessageDialog(null, "incorrect");
+	count-=1;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+String story =
+	"Your final score is "+count+" points";
+	JOptionPane.showMessageDialog(null, story);
 	}
 }
 
