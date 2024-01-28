c4f71d22aa9495f7ee492f0ffa7e8835f17b9045
league <karinabriellen@gmail.com>
2024-01-28 11:27:21 -0800

    finished robot worked on zombie eyes

--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -30,9 +30,29 @@ public class RobotInSpace extends KeyboardAdapter {
 	 */
 
 	private void moveRobot(int keyPressed) {
+		System.out.println(keyPressed);
+		
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
+		if (keyPressed == 38) {
+			rob.setAngle(0);
+			rob.move(10);
+		}
 		
+		if (keyPressed == 39) {
+			rob.setAngle(90);
+			rob.move(10);
+		}
+		
+		if (keyPressed == 40) {
+			rob.setAngle(180);
+			rob.move(10);
+		}
+		
+		if (keyPressed == 37) {
+			rob.setAngle(270);
+			rob.move(10);
+		}
 		// 3. If the up arrow is pressed, move the Robot up the screen.
 		
 		// 4. If the down arrow is pressed, move the Robot down.
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,20 @@
+PImage face;
 
 void setup() {
-
+  size(900,700);
+face = loadImage("face.jpg");
+ face.resize(900,700);
+ 
+ background(face);
 }
 void draw() {
+  image (face, 1,1);
 
+fill(152, 217, 106);
+ellipse(360, 265, 120, 50);
+ellipse(533, 265, 120, 50);
+
+fill(0, 0, 0);
+ellipse(360, 265, 70, 25);
+ellipse(533, 265, 70, 25);
 }
